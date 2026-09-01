61147a95eb4bb14f70b462e7acbc5a1a1a25a679
pmjpraveen <pmjprav@gmail.com>
2026-09-01 11:11:13 +0530

    Initial Commit

---
+++ suno/sunoApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunoApp.swift
+//  suno
+//
+//  Created by Praveenkumar Jogannavar on 01/09/26.
+//
+
+import SwiftUI
+
+@main
+struct sunoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
